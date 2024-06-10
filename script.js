c84b3e1b970f00ced2997f6948b286c7c352f21a
Francisco Agustín <franciscoagustinrd@gmail.com>
2024-06-09 22:42:27 -0400

    Renombre de id & clases

--- script.js
+++ script.js
@@ -7,27 +7,27 @@ precioSpan.innerHTML = precio;
 let btnMas = document.querySelector("#btnMas");
 let btnMenos = document.querySelector("#btnMenos");
 
-let cantidad = document.querySelector(".cantidad");
+let cantidad = document.querySelector("#cantidad");
 cantidad.innerHTML = numero;
 
 let valorTotal = document.querySelector(".valor-total");
 
 function total() {
-  valorTotal.innerHTML = precio * numero;
+ valorTotal.innerHTML = precio * numero;
 }
 
 btnMas.addEventListener("click", () => {
-  numero += 1;
-  cantidad.innerHTML = numero;
-  total();
+ numero += 1;
+ cantidad.innerHTML = numero;
+ total();
 });
 
 btnMenos.addEventListener("click", () => {
-  numero -= 1;
-  if (numero < 0) {
-    numero = 0;
-  }
+ numero -= 1;
+ if (numero < 0) {
+  numero = 0;
+ }
 
-  cantidad.innerHTML = numero;
-  total();
+ cantidad.innerHTML = numero;
+ total();
 });
